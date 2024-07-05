7fc8e545fa839cc3baa2088f19c455bf94f9e890
zyalwfie <zyalwfie@gmail>
2024-07-05 20:11:16 +0800

    Finish hero section and works section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./public/**/*.{html,js}"],
+	content: ['./public/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			tomato: '#f06449',
+			issabeline: '#ede6e3',
+			platinum: '#dadad9',
+			blackOlive: '#36382e',
+			vividSkyBlue: '#5bc3eb',
+		},
+		extend: {
+			fontFamily: {
+				federo: ['Federo'],
+				anonymousPro: ['Anonymous Pro'],
+				biryani: ['Biryani'],
+			},
+		},
 	},
 	plugins: [],
 };
