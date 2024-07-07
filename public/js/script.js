57d4db688026fbf58065697b8a297b7ddfae3478
zyalwfie <zyalwfie@gmail>
2024-07-07 19:51:01 +0800

    Finished the slicing

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,22 @@
-const sidebar = document.getElementById("sidebar");
-const hamburgerBtn = document.getElementById("hamburger");
-const closeBtn = document.getElementById("close-icon");
-const listBtn = document.getElementById("list-icon");
-const menuItems = document.querySelectorAll("#menu-item");
+const sidebar = document.getElementById('sidebar');
+const hamburgerBtn = document.getElementById('hamburger');
+const closeBtn = document.getElementById('close-icon');
+const listBtn = document.getElementById('list-icon');
+const menuItems = document.querySelectorAll('#menu-item');
 
 function toggleMenu() {
-	if (sidebar.classList.contains("translate-x-[-100%]")) {
-		sidebar.classList.remove("translate-x-[-100%]");
-		closeBtn.style.display = "block";
-		listBtn.style.display = "none";
+	if (sidebar.classList.contains('translate-x-[-100%]')) {
+		sidebar.classList.remove('translate-x-[-100%]');
+		closeBtn.style.display = 'block';
+		listBtn.style.display = 'none';
 	} else {
-		sidebar.classList.add("translate-x-[-100%]");
-		closeBtn.style.display = "none";
-		listBtn.style.display = "block";
+		sidebar.classList.add('translate-x-[-100%]');
+		closeBtn.style.display = 'none';
+		listBtn.style.display = 'block';
 	}
 }
 
-hamburgerBtn.addEventListener("click", toggleMenu);
+hamburgerBtn.addEventListener('click', toggleMenu);
 menuItems.forEach(function (menuItem) {
-	menuItem.addEventListener("click", toggleMenu);
+	menuItem.addEventListener('click', toggleMenu);
 });
