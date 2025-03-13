900128ca0ed9aebdfe0a5ad2236d43b381f6277e
wafy <zyalwfie@gmail.com>
2025-03-14 05:54:11 +0800

    fix: change position of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<footer
-		class="flex flex-col-reverse gap-y-2 fixed bottom-0 inset-x-0 px-4 md:px-8 py-4 bg-white lg:px-16 xl:px-64 shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1)] dark:text-white dark:bg-black">
+		class="flex flex-col-reverse gap-y-2 items-center px-4 py-6 md:px-8 lg:px-16 xl:px-64 dark:text-white bg-white dark:bg-black transition-all duration-300 shadow-[0_-10px_15px_-3px_rgba(0,0,0,0.1)]">
 		<div class="text-sm text-center">
 			&copy; 2025 Ziyad Alwafie. All rights reserved.
 		</div>
